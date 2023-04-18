b722c228f8164596fe97dc63c2d659d60f518eca
Aerilym <aerilymbiz@gmail.com>
2023-04-18 21:52:58 +1000

    remove index redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-'use client';
 import Link from 'next/link';
 import Layout from '@/app/components/template/Layout';
-import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-  router.push('/game');
   return (
     <Layout>
       <Link href="/game">Go To Demo</Link>
