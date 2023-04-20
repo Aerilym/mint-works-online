b87db1ac9c4f9d63a543a7aa1c7468931b46aa69
Ben Cook <mcbeanoo@gmail.com>
2023-04-20 22:24:16 +1000

    add deed color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,19 @@
+// https://www.tailwindshades.com/#color=158.57142857142856%2C16.470588235294112%2C66.66666666666667&step-up=8&step-down=11&hue-shift=0&name=summer-green&base-stop=5&v=1&overrides=e30%3D
+const deed = {
+  DEFAULT: '#9CB8AE',
+  50: '#FFFFFF',
+  100: '#FBFCFC',
+  200: '#E3EBE8',
+  300: '#CCDAD5',
+  400: '#B4C9C1',
+  500: '#9CB8AE',
+  600: '#7BA193',
+  700: '#5F8577',
+  800: '#48645A',
+  900: '#30433D',
+  950: '#25332E',
+};
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +21,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
-
+  darkMode: 'class',
   theme: {
     fontFamily: {
       sans: ['Poppins', 'sans-serif'],
@@ -17,7 +33,7 @@ module.exports = {
           textPrimary: '#1f3a42',
           textSecondary: '#f6eed8',
           border: '#5e5e5e',
-          deed: '#9cb8ae',
+          deed: deed,
           utility: '#f1b158',
           production: '#e17975',
           culture: '#b6c16c',
