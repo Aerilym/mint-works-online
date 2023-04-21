a1f48be7253c8a995d2b210bf6998b3b0497b7fd
Ben Cook <mcbeanoo@gmail.com>
2023-04-21 12:50:28 +1000

    Add basic lobby UI

---
+++ app/game/[gameId]/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@/app/components';
+
+interface PageParams {
+  params: {
+    gameId: string;
+  };
+}
+
+export default function Page({ params }: PageParams) {
+  const gameId = params.gameId.toLocaleUpperCase();
+
+  return (
+    <div className="flex flex-col gap-8">
+      <h1 className="mt-2 text-2xl">
+        Lobby <span className="rounded bg-red-500 p-2 shadow-xl">{gameId}</span>
+      </h1>
+      <Button>Start Game</Button>
+    </div>
+  );
+}
