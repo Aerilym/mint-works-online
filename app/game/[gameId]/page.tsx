6ad287f015f46aea73d065b5a3642abf7f5ecf2d
Aerilym <aerilymbiz@gmail.com>
2023-04-23 20:31:01 +1000

    Add game database select to game page

--- app/game/[gameId]/page.tsx
+++ app/game/[gameId]/page.tsx
@@ -1,20 +1,36 @@
 import { Button } from '@/components';
 
+import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { headers, cookies } from 'next/headers';
+
+import type { Database } from '@/lib/database.types';
+
 interface PageParams {
   params: {
     gameId: string;
   };
 }
 
-export default function Page({ params }: PageParams) {
-  const gameId = params.gameId.toLocaleUpperCase();
+export default async function Page({ params }: PageParams) {
+  const gameId = params.gameId;
+
+  const supabase = createServerComponentSupabaseClient<Database>({
+    headers,
+    cookies,
+  });
+
+  const { data: game, error } = await supabase
+    .from('game')
+    .select('*')
+    .eq('game_id', gameId)
+    .single();
 
   return (
     <div className="flex flex-col gap-8">
       <h1 className="mt-2 text-2xl">
-        Lobby <span className="rounded bg-red-500 p-2 shadow-xl">{gameId}</span>
+        Game <span className="rounded bg-red-500 p-2 shadow-xl">{gameId}</span>
       </h1>
-      <Button>Start Game</Button>
+      {game ? JSON.stringify(game) : error ? error.message : 'Loading...'}
     </div>
   );
 }
