fba60f674d5e576467d1857e32d12a654b331be7
Aerilym <aerilymbiz@gmail.com>
2023-04-24 14:50:07 +1000

    Update lobby to use live lobby

--- app/lobby/[joinCode]/page.tsx
+++ app/lobby/[joinCode]/page.tsx
@@ -1,9 +1,11 @@
+'use client';
+
 import { Button } from '@/components';
 
-import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { headers, cookies } from 'next/headers';
-
-import type { Database } from '@/lib/database.types';
+import Link from 'next/link';
+import { useSupabase } from '@/app/supabase-provider';
+import { useRouter } from 'next/navigation';
+import LiveLobby from './LiveLobby';
 
 interface PageParams {
   params: {
@@ -14,10 +16,9 @@ interface PageParams {
 export default async function Page({ params }: PageParams) {
   const joinCode = params.joinCode.toLocaleUpperCase();
 
-  const supabase = createServerComponentSupabaseClient<Database>({
-    headers,
-    cookies,
-  });
+  const router = useRouter();
+
+  const { supabase } = useSupabase();
 
   const { data: lobby, error } = await supabase
     .from('lobbies')
@@ -25,13 +26,63 @@ export default async function Page({ params }: PageParams) {
     .eq('join_code', joinCode.toLowerCase())
     .single();
 
+  const user = await supabase.auth.getUser();
+  const userId = user.data.user?.id;
+
+  const isLobbyOwner = lobby?.player_1 === userId;
+
+  const handleStartGame = async () => {
+    const res = await fetch(`/api/game`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        lobby_id: lobby?.id,
+      }),
+    });
+
+    const { game_id } = await res.json();
+
+    router.push(`/game/${game_id}`);
+  };
+
+  const handleJoinGame = async () => {
+    const { data, error } = await supabase
+      .from('lobbies')
+      .update({
+        player_2: userId,
+      })
+      .eq('id', lobby?.id);
+    if (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-8">
       <h1 className="mt-2 text-2xl">
         Lobby <span className="rounded bg-red-500 p-2 shadow-xl">{joinCode}</span>
       </h1>
-      {lobby ? JSON.stringify(lobby) : error ? error.message : 'Loading...'}
-      <Button>Start Game</Button>
+
+      <LiveLobby lobbyId={lobby!.id} initialLobby={lobby!} />
+
+      {userId ? (
+        isLobbyOwner ? (
+          <Button disabled>Join Game</Button>
+        ) : (
+          <Button onClick={handleJoinGame}>Join Game</Button>
+        )
+      ) : (
+        <Link href={'/login'}>
+          <Button>Login to Join</Button>
+        </Link>
+      )}
+      {isLobbyOwner ? (
+        <Button onClick={handleStartGame}>Start Game</Button>
+      ) : (
+        <Button disabled>Owner must start game</Button>
+      )}
     </div>
   );
 }
