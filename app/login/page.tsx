04bc20567d706a559062a6403d79c2c8a00f0bb0
Aerilym <aerilymbiz@gmail.com>
2023-04-25 18:57:07 +1000

    Add redirect after login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,14 +2,26 @@
 
 import { Auth } from '@supabase/auth-ui-react';
 import { useSupabase } from '@/app/supabase-provider';
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
+  const router = useRouter();
   const { supabase } = useSupabase();
+
+  supabase.auth.onAuthStateChange((event) => {
+    if (event === 'SIGNED_IN') {
+      router.push('/profile');
+    }
+  });
 
   return (
     <div className="flex flex-col">
       <h1 className="mt-2 self-center text-2xl">Login</h1>
-      <Auth supabaseClient={supabase} providers={['google', 'facebook', 'discord']} />
+      <Auth
+        supabaseClient={supabase}
+        providers={['google', 'facebook', 'discord']}
+        redirectTo="/profile"
+      />
     </div>
   );
 }
