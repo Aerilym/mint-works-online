3618f4f113601f0f2dd11a3eaa122fac43ef5f41
Aerilym <aerilymbiz@gmail.com>
2023-04-28 21:46:36 +1000

    Create basic game UI

--- app/game/[gameId]/LiveGame.tsx
+++ app/game/[gameId]/LiveGame.tsx
@@ -5,6 +5,9 @@ import { useEffect, useState } from 'react';
 import { useSupabase } from '@/app/supabase-provider';
 import { Turn } from 'mint-works/dist/turn';
 import type { Game } from '@/app/types/database';
+import { MintWorksEngineState } from 'mint-works/dist/mint_works';
+import { Plan } from '@/app/plan/Plan';
+import { Building, HandPlan } from 'mint-works/dist/plan';
 
 export default function LiveGame({
   gameId,
@@ -15,7 +18,7 @@ export default function LiveGame({
   initialGame: Game;
   playerName: string;
 }) {
-  const { liveGame, availableTurns } = useLiveGame(initialGame, gameId, playerName);
+  const { state, playerToTakeTurn, availableTurns } = useLiveGame(initialGame, gameId, playerName);
 
   const handleSendTurn = async (turn: Turn) => {
     const res = await fetch(`/api/turn/${gameId}`, {
@@ -37,21 +40,66 @@ export default function LiveGame({
   return (
     <div>
       <h1>Game {gameId}</h1>
-      <h2>Current turn: {liveGame.player_to_take_turn}</h2>
-      <h2>Available turns:</h2>
-      <ul>
-        {availableTurns.map((turn, i) => (
-          <li key={i}>
-            <button onClick={() => handleSendTurn(turn)}>{JSON.stringify(turn.action)}</button>
-          </li>
-        ))}
-      </ul>
+      <div className="grid w-screen grid-flow-row grid-cols-2">
+        <div>
+          <h2>Plan Supply:</h2>
+          <div className="flex h-80 flex-row gap-4">
+            {state.planSupply?.map((plan) => (
+              <Plan key={plan.name} plan={plan} />
+            ))}
+          </div>
+          <h2>Current turn: {playerToTakeTurn}</h2>
+          <h2>Available turns:</h2>
+          <ul>
+            {availableTurns.map((turn, i) => (
+              <li key={i}>
+                <button onClick={() => handleSendTurn(turn)}>{JSON.stringify(turn.action)}</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h2>Your neighbourhood:</h2>
+          <div>
+            <h3>{playerName}</h3>
+            <h4>{state.players?.find((player) => player.label === playerName)?.tokens} tokens</h4>
+            <Neighbourhood
+              neighbourhood={
+                state.players?.find((player) => player.label === playerName)?.neighbourhood!
+              }
+            />
+          </div>
+          <h2>Other neighbourhoods:</h2>
+          <div>
+            {state.players
+              ?.filter((player) => player.label !== playerName)
+              .map((player) => (
+                <div key={player.label}>
+                  <h3>{player.label}</h3>
+                  <h4>{player.tokens} tokens</h4>
+                  <Neighbourhood neighbourhood={player.neighbourhood} />
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-function useLiveGame(initialGame: Game, gameId: string, playerName: string) {
-  const [liveGame, setLiveGame] = useState<Game>(initialGame);
+function useLiveGame(
+  initialGame: Game,
+  gameId: string,
+  playerName: string
+): {
+  state: MintWorksEngineState;
+  playerToTakeTurn?: string;
+  availableTurns: Array<Turn>;
+} {
+  const [state, setLiveState] = useState<MintWorksEngineState>(
+    JSON.parse(initialGame.state as any) as MintWorksEngineState
+  );
+  const [playerToTakeTurn, setPlayerToTakeTurn] = useState<string | undefined>();
   const [availableTurns, setAvailableTurns] = useState<Array<Turn>>([]);
 
   const { supabase } = useSupabase();
@@ -62,7 +110,13 @@ function useLiveGame(initialGame: Game, gameId: string, playerName: string) {
         method: 'GET',
       });
 
-      const { valid_turns } = await res.json();
+      const { valid_turns } = (await res.json()) as { valid_turns: Array<Turn> };
+
+      if (!res.ok) throw new Error('Error fetching turns!');
+
+      if (valid_turns.length === 0) throw new Error('No valid turns!');
+
+      if (valid_turns[0].playerName !== playerName) return;
 
       setAvailableTurns(valid_turns);
     };
@@ -78,7 +132,8 @@ function useLiveGame(initialGame: Game, gameId: string, playerName: string) {
         },
         (payload) => {
           console.log('Change received!', payload);
-          setLiveGame(payload.new as Game);
+          setLiveState(payload.new.state as any);
+          setPlayerToTakeTurn(payload.new.player_to_take_turn);
 
           if (payload.new.player_to_take_turn === playerName) {
             handleUpdateTurns();
@@ -87,10 +142,40 @@ function useLiveGame(initialGame: Game, gameId: string, playerName: string) {
       )
       .subscribe();
 
+    handleUpdateTurns();
+
     return () => {
       supabase.removeChannel(channel);
     };
   }, [gameId, playerName, supabase]);
 
-  return { liveGame, availableTurns };
+  return {
+    state,
+    playerToTakeTurn,
+    availableTurns,
+  };
+}
+
+function Neighbourhood({
+  neighbourhood,
+}: {
+  neighbourhood: {
+    plans: Array<HandPlan>;
+    buildings: Array<Building>;
+  };
+}) {
+  return (
+    <div className="flex flex-col">
+      <div className="flex h-80 flex-row gap-4">
+        {neighbourhood?.plans.map((plan) => (
+          <Plan key={plan.name} plan={plan} />
+        ))}
+      </div>
+      <div className="flex h-80 flex-row gap-4">
+        {neighbourhood?.buildings.map((building) => (
+          <Plan key={building.name} plan={building} />
+        ))}
+      </div>
+    </div>
+  );
 }
