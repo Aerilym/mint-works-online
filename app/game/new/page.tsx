d003871b63cefaf5a048219ebdee3418c121cc19
Aerilym <aerilymbiz@gmail.com>
2023-05-03 11:00:35 +1000

    Move lobby creation logic to api route

--- app/game/new/page.tsx
+++ app/game/new/page.tsx
@@ -1,8 +1,9 @@
 import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import Hashids from 'hashids';
 import { cookies, headers } from 'next/headers';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
+import { createLobby } from '@/app/api/lobby/route';
 import { Button } from '@/components';
 import type { Database } from '@/lib/database.types';
 
@@ -16,28 +17,13 @@ export default async function Page() {
 
   const userId = user.data.user?.id;
 
-  const row = {
-    player_1: userId,
-  };
+  if (!userId) redirect('/login');
 
-  const { data: lobby, error } = await supabase.from('lobbies').insert([row]).select('*').single();
-
-  let joinCode = '';
-  if (lobby) {
-    //pad lobby id to 6 digits
-    const paddedId = lobby.id.toString().padStart(6, '0');
-
-    const hashids = new Hashids('Mint Works', 6, 'abcdefghikmnopqrstuvwxyz23456789');
-    joinCode = hashids.encode('69', paddedId);
-
-    await supabase.from('lobbies').update({ join_code: joinCode }).eq('id', lobby.id);
-  }
+  const joinCode = await createLobby({ userId });
 
   return (
     <div>
-      <div>{userId ? userId : 'Loading...'}</div>
-      <div>{row ? JSON.stringify(row) : 'Loading...'}</div>
-      <div>{lobby ? JSON.stringify(lobby) : error ? error.message : 'Loading...'}</div>
+      <h1>Game Created</h1>
       <Link href={`/lobby/${joinCode}`}>
         <Button>Go to lobby</Button>
       </Link>
